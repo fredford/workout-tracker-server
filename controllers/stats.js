be4fd2ff314a9758b008194581c8a657e1815b39
Fraser Redford <57697667+fredford@users.noreply.github.com>
2022-06-26 15:59:39 -0600

    updated exercise stats data API

--- controllers/stats.js
+++ controllers/stats.js
@@ -42,6 +42,7 @@ export const getExerciseData = async (req, res, next) => {
     var dateArr = [];
 
     var cumulativeSum = 0;
+    var setCounter = 1;
 
     for (const set of sets) {
       var date = new Date(set.date)
@@ -52,18 +53,34 @@ export const getExerciseData = async (req, res, next) => {
 
       var amount = Number(set.amount);
 
-      console.log(cumulativeSum);
       if (index == -1) {
         dateArr.push(date);
         output.workoutProgression[date] = amount;
+
+        // Summing the cumulative total repetitions
         cumulativeSum += amount;
 
+        // Setting the current cumulative total on the new date
         output.cumulative[date] = cumulativeSum;
+
+        // Set Progression Counter
+        setCounter = 1;
       } else {
+        // Summing the repetitions done on this date
         output.workoutProgression[date] += amount;
+
+        // Summing the repetitions amount on each date
         cumulativeSum += amount;
+
+        // Updating the current cumulative total on the date
         output.cumulative[date] += amount;
+
+        // Set Progression Counter
+        setCounter += 1;
       }
+
+      // Setting the current Set Count with the amount done
+      output.setProgression[`${date} Set ${setCounter}`] = amount;
     }
 
     res.status(200).json({ success: true, data: output });
