03448fa4bec77dfe8bbd2daacd3f59f2f0d18c1c
Fraser Redford <57697667+fredford@users.noreply.github.com>
2022-07-06 00:21:52 -0600

    added AdminJS dashboard
    added user authentication so only admin@admin.com can access the admin page

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 import express from "express";
 import cors from "cors";
+
+import mongoose from "mongoose";
+
+import AdminJS from "adminjs";
+import AdminJSExpress from "@adminjs/express";
+import AdminJSMongoose from "@adminjs/mongoose";
+
 import dotenv from "dotenv";
 
-import connectDB from "./config/db.js";
+import bcrypt from "bcryptjs";
 import routes from "./routes/routes.js";
 import privateRoutes from "./routes/private.js";
 import userRoutes from "./routes/user.js";
@@ -13,17 +20,59 @@ import setRoutes from "./routes/sets.js";
 import statsRoutes from "./routes/stats.js";
 
 import errorHandler from "./middleware/error.js";
+import User from "./models/User.js";
+import router from "./routes/sets.js";
 
 dotenv.config({ path: "./.env" });
 
-// Connect to MongoD
-connectDB();
-
 const app = express();
+
+// Register the Mongoose Adapter
+AdminJS.registerAdapter(AdminJSMongoose);
+
+// Run AdminJS with Mongoose
+const connectDB = async () => {
+  const mongooseDb = await mongoose.connect(process.env.WORKOUTTRACKER_DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  const adminJs = new AdminJS({ databases: [mongooseDb], rootPath: "/admin" });
+  const adminJSrouter = AdminJSExpress.buildAuthenticatedRouter(
+    adminJs,
+    {
+      authenticate: async (email, password) => {
+        if (email === "admin@admin.com") {
+          const user = await User.findOne({ email: "admin@admin.com" }).select(
+            "+password"
+          );
+
+          const isMatch = await user.matchPasswords(password);
+
+          if (isMatch) {
+            return user;
+          }
+          return false;
+        }
+      },
+      cookiePassword: "some-secret-password",
+    },
+    null,
+    {
+      resave: false,
+      saveUninitialized: true,
+    }
+  );
+
+  app.use(adminJs.options.rootPath, adminJSrouter);
+  console.log("MongoDB connected");
+};
+
+// Connect to MongoDB
+connectDB();
 
 app.use(cors());
 app.use(express.json());
-
 app.use("/api/v1", routes);
 app.use("/api/v1/private", privateRoutes);
 app.use("/api/v1", userRoutes);
@@ -39,7 +88,8 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 8000;
 
 const server = app.listen(
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`)),
+  app.listen(8080, () => console.log("AdminJS is under localhost:8080/admin"))
 );
 
 process.on("unhandledRejection", (err, promise) => {
