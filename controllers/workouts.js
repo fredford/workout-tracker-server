bd2134e715bc6f9341d35cd5657feb9c4df12b09
Fraser Redford <57697667+fredford@users.noreply.github.com>
2022-07-21 03:05:37 -0600

    add query for last workout started
    added a query for Workouts to provide the last workout started and compute the total sets and repetitions done

--- controllers/workouts.js
+++ controllers/workouts.js
@@ -2,6 +2,7 @@ import jwt from "jsonwebtoken";
 
 import Workout from "../models/Workout.js";
 import User from "../models/User.js";
+import Set from "../models/Set.js";
 
 export const getWorkouts = async (req, res, next) => {
   const token = req.headers.authorization.split(" ")[1];
@@ -13,6 +14,7 @@ export const getWorkouts = async (req, res, next) => {
 
     query.push(decoded.id);
 
+    // Query Parameter for ID is passed, using getById
     if (req.query.id) {
       const workout = await Workout.find({
         user: decoded.id,
@@ -26,6 +28,36 @@ export const getWorkouts = async (req, res, next) => {
         next(error);
       }
       res.status(200).json({ success: true, data: workout });
+    }
+    // Query Parameter for Last is passed, using getLast. Looking for the most recent workout
+    else if (req.query.last) {
+      // Query the database for the latest workout performed by the user
+      const workout = await Workout.find({ user: decoded.id })
+        .sort({ date: -1 })
+        .limit(1);
+
+      // Check that a workout is returned
+      if (workout.length === 0) {
+        res.stats(404).json({ success: false, data: "Workout does not exist" });
+        next(error);
+      }
+
+      // Get the list of sets for this workout
+      const sets = await Set.find({ workout: workout[0]._id });
+      // Compute the number of sets performed
+      let setsCount = sets.length;
+      // Calculate the total number of repetitions performed
+      let totalReps = sets.reduce((sum, curr) => sum + Number(curr.amount), 0);
+      // Return the data object
+      res.status(200).json({
+        success: true,
+        data: {
+          id: workout[0]._id,
+          type: workout[0].type,
+          sets: setsCount,
+          totalRepetitions: totalReps,
+        },
+      });
     } else {
       const workouts = await Workout.find({
         user: decoded.id,
